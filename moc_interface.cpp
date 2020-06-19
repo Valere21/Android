18936a2c06a14d4e9c6811c23317629426e388ea
Valere21 <valere.gh@hotmail.fr>
2020-06-19 16:22:52 -0700

    debug + build

---
+++ moc_interface.cpp
@@ -0,0 +1,133 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'interface.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.12.5)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "interface.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'interface.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.12.5. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_Interface_t {
+    QByteArrayData data[9];
+    char stringdata0[64];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_Interface_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_Interface_t qt_meta_stringdata_Interface = {
+    {
+QT_MOC_LITERAL(0, 0, 9), // "Interface"
+QT_MOC_LITERAL(1, 10, 15), // "connectToServer"
+QT_MOC_LITERAL(2, 26, 0), // ""
+QT_MOC_LITERAL(3, 27, 2), // "ip"
+QT_MOC_LITERAL(4, 30, 4), // "port"
+QT_MOC_LITERAL(5, 35, 11), // "viewChanger"
+QT_MOC_LITERAL(6, 47, 2), // "id"
+QT_MOC_LITERAL(7, 50, 8), // "loadMain"
+QT_MOC_LITERAL(8, 59, 4) // "init"
+
+    },
+    "Interface\0connectToServer\0\0ip\0port\0"
+    "viewChanger\0id\0loadMain\0init"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_Interface[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       4,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    2,   34,    2, 0x0a /* Public */,
+       5,    1,   39,    2, 0x08 /* Private */,
+       7,    0,   42,    2, 0x08 /* Private */,
+       8,    0,   43,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void, QMetaType::QString, QMetaType::Int,    3,    4,
+    QMetaType::Void, QMetaType::Int,    6,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void Interface::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<Interface *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->connectToServer((*reinterpret_cast< QString(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2]))); break;
+        case 1: _t->viewChanger((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 2: _t->loadMain(); break;
+        case 3: _t->init(); break;
+        default: ;
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject Interface::staticMetaObject = { {
+    &QQuickView::staticMetaObject,
+    qt_meta_stringdata_Interface.data,
+    qt_meta_data_Interface,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *Interface::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *Interface::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_Interface.stringdata0))
+        return static_cast<void*>(this);
+    return QQuickView::qt_metacast(_clname);
+}
+
+int Interface::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QQuickView::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 4)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 4;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 4)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 4;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
